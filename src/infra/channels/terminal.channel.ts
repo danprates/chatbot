d5ffa6a20c839a42d5d8542dfb7b8562b3c63a29
Daniel Prates <danprates94@gmail.com>
2023-06-07 10:04:29 -0300

    refactor(channel): move terminal to a separated file

---
+++ src/infra/channels/terminal.channel.ts
@@ -0,0 +1,31 @@
+import { Domain } from "@/domain/domain.protocol";
+import readline from "node:readline";
+
+export class Terminal implements Domain.Channel {
+  private readonly rl: readline.Interface;
+  constructor() {
+    this.rl = readline.createInterface({
+      input: process.stdin,
+      output: process.stdout,
+    });
+  }
+  open(onMessage: (text: string) => string[]) {
+    this.rl.prompt();
+    this.rl.on("line", (userInput: string) => {
+      const messages = onMessage(userInput);
+      this.write(messages);
+      this.rl.prompt();
+    });
+    this.rl.on("close", () => {
+      process.exit(0);
+    });
+  }
+  close() {
+    this.rl.close();
+  }
+  write(messages: string[]) {
+    for (const message of messages) {
+      console.log("BOT:", message);
+    }
+  }
+}
