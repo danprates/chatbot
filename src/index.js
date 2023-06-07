9863910a70e2ab468c31ba9386e304a7728e3692
Daniel Prates <danprates94@gmail.com>
2023-06-06 21:14:03 -0300

    feat: add terminal to index

--- src/index.js
+++ src/index.js
@@ -1,14 +1,34 @@
 const { BayesClassifier } = require("natural");
 const raw = require("./classifier.json");
+const readline = require("readline");
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
 
 function main() {
   const classifier = BayesClassifier.restore(raw);
 
-  // now ask it to categorize a document it has never seen before
-  const result = classifier.classify("awesome, cool, amazing!! Yay.");
+  console.log('Welcome to the Chatbot! Type "exit" to end the conversation.');
 
-  // => 'positive'
-  console.log("result", result);
+  rl.prompt();
+
+  rl.on("line", (userInput) => {
+    if (userInput.toLowerCase() === "sair") {
+      console.log("Thank you for using the Chatbot. Goodbye!");
+      rl.close();
+    } else {
+      const result = classifier.classify(userInput);
+      console.log(result);
+
+      rl.prompt();
+    }
+  });
+
+  rl.on("close", () => {
+    process.exit(0);
+  });
 }
 
 main();
