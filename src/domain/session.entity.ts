d0165fc7b5cbb5a0a951d5816cd57d899d21eb4f
Daniel Prates <danprates94@gmail.com>
2023-06-07 10:18:15 -0300

    feat(session): add Session entity

---
+++ src/domain/session.entity.ts
@@ -0,0 +1,19 @@
+import { Message } from "./message.entity";
+
+export class Session {
+  private readonly startedAt: Date;
+  private finishedAt?: Date;
+  private readonly messages: Message[];
+  constructor() {
+    this.startedAt = new Date();
+    this.messages = [];
+  }
+
+  addMessage(message: Message) {
+    this.messages.push(message);
+  }
+
+  finish() {
+    this.finishedAt = new Date();
+  }
+}
