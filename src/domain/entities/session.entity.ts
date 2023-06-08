3cad2a16850a44bcf65c01abe800c9914c3e00cc
Daniel Prates <danprates94@gmail.com>
2023-06-08 10:04:59 -0300

    feat(session): add waiting for response

--- src/domain/entities/session.entity.ts
+++ src/domain/entities/session.entity.ts
@@ -4,9 +4,11 @@ export class Session {
   private readonly startedAt: Date;
   private finishedAt?: Date;
   private readonly messages: Message[];
+  private waitingForResponse: boolean;
   constructor(private readonly user: User) {
     this.startedAt = new Date();
     this.messages = [];
+    this.waitingForResponse = false;
   }
 
   addMessage(message: Message) {
@@ -15,5 +17,13 @@ export class Session {
 
   finish() {
     this.finishedAt = new Date();
+  }
+
+  waitForResponse() {
+    this.waitingForResponse = true;
+  }
+
+  isWaitingForResponse(): boolean {
+    return this.waitingForResponse;
   }
 }
